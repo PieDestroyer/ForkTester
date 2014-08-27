b99249d188d705e53535b35d3bc825c29643418f
Travis.Day <Travis.Day@MU-C3-18.mullen.pvt.k12.co.us>
2014-08-27 09:35:55 -0600

    Syso from school

--- Fork Tester/src/ForkTester.java
+++ Fork Tester/src/ForkTester.java
@@ -6,9 +6,10 @@ public class ForkTester
 		// Try what you did before.  
 		// Fork this to your personal GitHub account.
 		// Download it to school and add a line.
-		System.out.println("This line is added from home.");
+		System.out.println("I added this line from school");
 		// Commit that back to your GitHub account.
 		// Download it to your home local repo.
+		System.out.println("This line is added from home.");
 		// Commit that back to GitHub.
 		// Update your local school repo and show me.
 
